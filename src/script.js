a953349cd8222f12998731ed67bb63c1bf10ee65
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-20 15:08:28 +0200

    add variables, add footer, add header

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// /************ BURGER MENU ************/
+
+// var burger = document.querySelector(".container");
+// var menu = document.querySelector(".menu");
+// var header = document.querySelector(".header");
+
+// burger.addEventListener("click", function() {
+//   burger.classList.toggle("is-click");
+//   menu.classList.toggle("is-open-menu");
+//   header.classList.toggle("header-is-open");
+// });
