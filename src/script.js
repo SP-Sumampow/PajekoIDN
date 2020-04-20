4d8bb68dd9c0bbcde03ef03c7b78fef2639c2995
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-20 23:39:30 +0200

    activate meu, style mainpage

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 // /************ BURGER MENU ************/
 
-// var burger = document.querySelector(".container");
-// var menu = document.querySelector(".menu");
-// var header = document.querySelector(".header");
+var burger = document.querySelector(".container");
+var menu = document.querySelector(".menu");
+var header = document.querySelector(".header");
 
-// burger.addEventListener("click", function() {
-//   burger.classList.toggle("is-click");
-//   menu.classList.toggle("is-open-menu");
-//   header.classList.toggle("header-is-open");
-// });
+burger.addEventListener("click", function () {
+  burger.classList.toggle("is-click");
+  menu.classList.toggle("is-open-menu");
+  header.classList.toggle("header-is-open");
+});
